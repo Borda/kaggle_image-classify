e00a54336750171f25ede6bde909b47e10480ea4
Jirka Borovec <Borda@users.noreply.github.com>
2021-04-02 20:22:32 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Kaggle sandbox
+
+![CI complete testing](https://github.com/Borda/kaggle_sandbox/workflows/CI%20complete%20testing/badge.svg?branch=main&event=push)
+![Check Code formatting](https://github.com/Borda/kaggle_sandbox/workflows/Check%20Code%20formatting/badge.svg?branch=main&event=push)
+
+This is a simple template project to Kaggle participation...
+
+### Expected content:
+- a core package with functionalities used across scripts and notebooks
+- tutorial/experiment notebooks (in _notebook_ folder if needed)
+- [optional] some functionality testing aka unittests or doctests
+
+### What to write here:
+- simple completion description + link
+- shortly what task it is to solve
+- list/link online sources
+- link kernels/submissions
---
+++ setup.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+
+import os
+
+# Always prefer setuptools over distutils
+from setuptools import find_packages, setup
+
+# https://packaging.python.org/guides/single-sourcing-package-version/
+# http://blog.ionelmc.ro/2014/05/25/python-packaging/
+
+_PATH_ROOT = os.path.dirname(__file__)
+
+import challenge_xyz  # noqa: E402
+
+
+def _load_requirements(path_dir=_PATH_ROOT, comment_char='#'):
+    with open(os.path.join(path_dir, 'requirements.txt'), 'r') as file:
+        lines = [ln.strip() for ln in file.readlines()]
+    reqs = [ln[:ln.index(comment_char)] if comment_char in ln else ln for ln in lines]
+    reqs = [ln for ln in reqs if ln]
+    return reqs
+
+
+def _load_long_description():
+    url = os.path.join(challenge_xyz.__homepage__, 'raw', challenge_xyz.__version__, 'docs')
+    text = open('README.md', encoding='utf-8').read()
+    # replace relative repository path to absolute link to the release
+    text = text.replace('](docs', f']({url}')
+    # SVG images are not readable on PyPI, so replace them  with PNG
+    text = text.replace('.svg', '.png')
+    return text
+
+
+# https://packaging.python.org/discussions/install-requires-vs-requirements /
+# keep the meta-data here for simplicity in reading this file... it's not obvious
+# what happens and to non-engineers they won't know to look in init ...
+# the goal of the project is simplicity for researchers, don't want to add too much
+# engineer specific practices
+setup(
+    name='kaggle-sandbox',
+    version=challenge_xyz.__version__,
+    description=challenge_xyz.__docs__,
+    author=challenge_xyz.__author__,
+    author_email=challenge_xyz.__author_email__,
+    url=challenge_xyz.__homepage__,
+    license=challenge_xyz.__license__,
+    packages=find_packages(exclude=['tests', 'docs']),
+    long_description=_load_long_description(),
+    long_description_content_type='text/markdown',
+    include_package_data=True,
+    zip_safe=False,
+    keywords=['deep learning', 'pytorch', 'AI'],
+    python_requires='>=3.6',
+    setup_requires=[],
+    install_requires=_load_requirements(_PATH_ROOT),
+    project_urls={
+        "Source Code": challenge_xyz.__homepage__,
+    },
+    classifiers=[
+        'Environment :: Console',
+        'Natural Language :: English',
+        # How mature is this project? Common values are
+        #   3 - Alpha, 4 - Beta, 5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Scientific/Engineering :: Image Recognition',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        # Pick your license as you wish
+        # 'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 3',
+    ],
+)
