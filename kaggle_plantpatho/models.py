a4738f6ab9c116f98b28d2a23c04db8e30fd458d
Jirka Borovec <jirka.borovec@seznam.cz>
2021-04-09 11:40:21 +0200

    fix

--- kaggle_plantpatho/models.py
+++ kaggle_plantpatho/models.py
@@ -83,7 +83,7 @@ class MultiPlantPathology(LitPlantPathology):
     def __init__(self, model, lr: float = 1e-4, loss_weight: Optional[Tensor] = None):
         super().__init__(model, lr)
         self.val_f1_score = torchmetrics.F1(self.num_classes, multilabel=True, average='weighted')
-        self.loss = nn.BCEWithLogitsLoss(weight=loss_weight)
+        self.loss = nn.BCEWithLogitsLoss(pos_weight=loss_weight)
 
     def forward(self, x: Tensor) -> Tensor:
         return F.sigmoid(self.model(x))
