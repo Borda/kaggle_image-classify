6101837fd0aac8922dc72d4b2f5abfc2a0790ab5
Jirka Borovec <jirka.borovec@seznam.cz>
2021-04-14 11:50:16 +0200

    path

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ def _load_requirements(path_dir=_PATH_ROOT, comment_char='#'):
     with open(os.path.join(path_dir, 'requirements.txt'), 'r') as file:
         lines = [ln.strip() for ln in file.readlines()]
     reqs = [ln[:ln.index(comment_char)] if comment_char in ln else ln for ln in lines]
-    reqs = [ln for ln in reqs if ln]
+    reqs = [ln for ln in reqs if ln and not any(s in ln for s in ['http://', 'https://'])]
     return reqs
 
 
