c8065f21abf7cceb3f5f9b6abc46bc71ca2bfd46
Jirka <jirka.borovec@seznam.cz>
2021-06-06 23:05:53 +0200

    port setup.cfg

--- kaggle_plantpatho/__init__.py
+++ kaggle_plantpatho/__init__.py
@@ -4,8 +4,6 @@ __version__ = "0.5.0"
 __docs__ = "Tooling for Kaggle Plant Pathology"
 __author__ = "Jiri Borovec"
 __author_email__ = "jirka@pytorchlightning.ai"
-__homepage__ = "https://github.com/Borda/kaggle_plant-pathology"
-__license__ = "MIT"
 
 _PATH_PACKAGE = os.path.realpath(os.path.dirname(__file__))
 _PATH_PROJECT = os.path.dirname(_PATH_PACKAGE)
